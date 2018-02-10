8c2fce0e4cf28d4c4219e483f91f35c6affaae49
Michael Ströder <michael@stroeder.com>
2018-02-10 14:28:32 +0100

    explicitly use python2

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python setup.py clean --all
+python2 setup.py clean --all
 rm -r MANIFEST .coverage dist/ldap0* build/* *.egg-info .tox docs/.build/*
 rm _libldap0.so _libldap0.cpython*.so ldap0/*.py? ldap0/*/*.py? tests/*.py? *.py?
 find -name __pycache__ | xargs -n1 -iname rm -r name
