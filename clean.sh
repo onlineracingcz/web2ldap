8e8a16dba91152200f27ee7302c3663a3ae029de
Michael Ströder <michael@stroeder.com>
2018-02-13 22:26:12 +0100

    delete *.py? in clean.sh

--- clean.sh
+++ clean.sh
@@ -3,5 +3,6 @@
 python2 setup.py clean --all
 rm -r MANIFEST .coverage dist/ldap0* build/* *.egg-info .tox docs/.build/*
 rm _libldap0.so _libldap0.cpython*.so ldap0/*.py? ldap0/*/*.py? tests/*.py? *.py?
+find -name "*.py?" -delete
 find -name __pycache__ | xargs -n1 -iname rm -r name
 rm -r slapdtest-[0-9]*
