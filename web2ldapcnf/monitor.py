b136ad10a24af54930624d546a83d883543c5bad
Michael Ströder <michael@stroeder.com>
2018-02-22 00:57:34 +0100

    comment

--- web2ldapcnf/monitor.py
+++ web2ldapcnf/monitor.py
@@ -10,7 +10,7 @@ Note that these options does not affect anything else than web2ldap/monitor
 
 # List of accepted address/net mask strings of
 # accepted client addresses.
-# Use ['0.0.0.0/0.0.0.0','::0'] to allow access to every client but think twice!
+# Use [u'0.0.0.0/0.0.0.0','::0'] to allow access to every client but think twice!
 # IPv6 network addresses without brackets!
 access_allowed = [
   u'127.0.0.0/255.0.0.0',
