72784f3cc9f28bdc85766639629e5b05a3e1bbfa
Michael Ströder <michael@stroeder.com>
2018-02-27 22:08:27 +0100

    web2ldap.ldaputil.passwd now PEP-8 compliant

--- web2ldap/ldaputil/passwd.py
+++ web2ldap/ldaputil/passwd.py
@@ -21,60 +21,69 @@ import hashlib
 from ldap0.pw import random_string, PWD_OCTETS_ALPHABET, PWD_UNIX_CRYPT_ALPHABET
 
 AVAIL_USERPASSWORD_SCHEMES = {
-  'sha':'SHA-1',
-  'ssha':'salted SHA-1',
-  'md5':'MD5',
-  'smd5':'salted MD5',
-  'sha256':'SHA-256',
-  'ssha256':'salted SHA-256',
-  'sha384':'SHA-384',
-  'ssha384':'salted SHA-384',
-  'sha512':'SHA-512',
-  'ssha512':'salted SHA-512',
-  '':'plain text',
+    'sha':'SHA-1',
+    'ssha':'salted SHA-1',
+    'md5':'MD5',
+    'smd5':'salted MD5',
+    'sha256':'SHA-256',
+    'ssha256':'salted SHA-256',
+    'sha384':'SHA-384',
+    'ssha384':'salted SHA-384',
+    'sha512':'SHA-512',
+    'ssha512':'salted SHA-512',
+    '':'plain text',
 }
-
 try:
-  import crypt
+    import crypt
 except ImportError:
-  pass
+    pass
 else:
-  AVAIL_USERPASSWORD_SCHEMES['crypt'] = 'Unix crypt(3)'
+    AVAIL_USERPASSWORD_SCHEMES['crypt'] = 'Unix crypt(3)'
 
-SCHEME2HASHLIBFUNC = {
-  'sha':hashlib.sha1,
-  'ssha':hashlib.sha1,
-  'md5':hashlib.md5,
-  'smd5':hashlib.md5,
-  'sha256':hashlib.sha256,
-  'ssha256':hashlib.sha256,
-  'sha384':hashlib.sha384,
-  'ssha384':hashlib.sha384,
-  'sha512':hashlib.sha512,
-  'ssha512':hashlib.sha512,
+
+SALTED_USERPASSWORD_SCHEMES = {
+    'smd5',
+    'ssha',
+    'ssha256',
+    'ssha384',
+    'ssha512',
 }
 
 
-def user_password_hash(password,scheme,salt=None):
+# map lower-cased password scheme to hash function
+SCHEME2HASHLIBFUNC = {
+    'sha': hashlib.sha1,
+    'ssha': hashlib.sha1,
+    'md5': hashlib.md5,
+    'smd5': hashlib.md5,
+    'sha256': hashlib.sha256,
+    'ssha256': hashlib.sha256,
+    'sha384': hashlib.sha384,
+    'ssha384': hashlib.sha384,
+    'sha512': hashlib.sha512,
+    'ssha512': hashlib.sha512,
+}
+
+
+def user_password_hash(password, scheme, salt=None):
     """
     Return hashed password (including salt).
     """
     scheme = scheme.lower().strip()
     if not scheme:
         return password
-    if not scheme in AVAIL_USERPASSWORD_SCHEMES.keys():
-        raise ValueError,'Hashing scheme %r not supported.' % (scheme)
-    if salt is None:
-        if scheme=='crypt':
-            salt = random_string(PWD_UNIX_CRYPT_ALPHABET,2)
-        elif scheme in ('smd5','ssha','ssha256','ssha384','ssha512'):
-            salt = random_string(PWD_OCTETS_ALPHABET,12)
-        else:
-            salt = ''
-    if scheme=='crypt':
-        encoded_pw = crypt.crypt(password,salt)
+    if scheme not in AVAIL_USERPASSWORD_SCHEMES.keys():
+        raise ValueError, 'Hashing scheme %r not supported.' % (scheme)
+    if scheme == 'crypt':
+        encoded_pw = crypt.crypt(
+            password,
+            random_string(PWD_UNIX_CRYPT_ALPHABET, 2)
+        )
     elif SCHEME2HASHLIBFUNC.has_key(scheme):
-        encoded_pw = base64.encodestring(SCHEME2HASHLIBFUNC[scheme](password+salt).digest()+salt).strip().replace('\n','')
+        salt = random_string(PWD_OCTETS_ALPHABET, 12)
+        encoded_pw = base64.encodestring(
+            SCHEME2HASHLIBFUNC[scheme](password+salt).digest()+salt
+        ).strip().replace('\n', '')
     else:
         encoded_pw = password
-    return '{%s}%s' % (scheme.upper(),encoded_pw)
+    return '{%s}%s' % (scheme.upper(), encoded_pw)
