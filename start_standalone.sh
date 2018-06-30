8686d0fc43e0dd61c0d821c485fc0cd8aa2a66a5
Michael Ströder <michael@stroeder.com>
2018-06-30 08:44:51 +0200

    simple start script can be invoked from outside the working directory

--- start_standalone.sh
+++ start_standalone.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-WEB2LDAP_HOME="$(pwd)" PYTHONPATH="$(pwd)" python2 "$(pwd)/web2ldap/wsgi.py"
+WEB2LDAP_HOME="$(dirname $0)"
+if [ ${WEB2LDAP_HOME} = "." ]
+then
+  WEB2LDAP_HOME="$(pwd)"
+fi
+export WEB2LDAP_HOME
+
+PYTHONPATH="${WEB2LDAP_HOME}" python2 "${WEB2LDAP_HOME}/web2ldap/wsgi.py"
