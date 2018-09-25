0052f4f5ec68be8967b7e1968bb183aa5201680f
Michael Ströder <michael@stroeder.com>
2018-09-25 19:11:49 +0200

    ignoring ValueError in macOS needs ldap0>=0.0.62 to be really helpful

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     data_files=data_files,
     install_requires=[
         'setuptools',
-        'ldap0>=0.0.60',
+        'ldap0>=0.0.62',
         'pyweblib',
         'ipaddress',
         'asn1crypto',
