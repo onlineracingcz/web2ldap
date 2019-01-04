1e5852cc2e5012460106cded88103549b16eebee
Michael Ströder <michael@stroeder.com>
2019-01-04 21:27:47 +0100

    new mix-in class web2ldap.log.LogHelper

--- web2ldap/log.py
+++ web2ldap/log.py
@@ -12,6 +12,18 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', logging.INFO)
 LOG_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
 
 
+class LogHelper:
+  """
+  mix-in class for logging with a class-specific log prefix
+  """
+
+  def _log_prefix(self):
+      return '%s[%x] ' % (self.__class__.__name__, id(self))
+
+  def log(self, level, msg, *args, **kwargs):
+      logger.log(level, ' '.join((self._log_prefix(), msg)), *args, **kwargs)
+
+
 def init_logger():
     """
     Create logger instance
