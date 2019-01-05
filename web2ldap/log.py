dc5e8aab1d423f1bb7f9d58a8be9383682839b53
Michael Ströder <michael@stroeder.com>
2019-01-05 16:00:58 +0100

    added full boiler plate to web2ldap.log.__doc__

--- web2ldap/log.py
+++ web2ldap/log.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 web2ldap.log -- Logging
+
+web2ldap - a web-based LDAP Client,
+see https://www.web2ldap.de for details
+
+(c) 1998-2019 by Michael Stroeder <michael@stroeder.com>
+
+This software is distributed under the terms of the
+Apache License Version 2.0 (Apache-2.0)
+https://www.apache.org/licenses/LICENSE-2.0
 """
 
 from __future__ import absolute_import
