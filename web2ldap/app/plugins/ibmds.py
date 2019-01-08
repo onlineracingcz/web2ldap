fff5ad8bfdc1abbb2e0121acdf248623910fe2ab
Michael Ströder <michael@stroeder.com>
2019-01-08 12:07:21 +0100

    web2ldap.app.plugins.ibmds: PEP-8

--- web2ldap/app/plugins/ibmds.py
+++ web2ldap/app/plugins/ibmds.py
@@ -5,27 +5,27 @@ web2ldap plugin classes for IBM Tivoliy Directory Server
 
 from __future__ import absolute_import
 
-from web2ldap.app.schema.syntaxes import syntax_registry,OID,DistinguishedName,OctetString
+from web2ldap.app.schema.syntaxes import syntax_registry, OID, DistinguishedName, OctetString
 
 syntax_registry.registerAttrType(
-  OID.oid, [
-    '1.3.18.0.2.4.2482', # ibm-enabledCapabilities
-    '1.3.18.0.2.4.2481', # ibm-supportedCapabilities
-    'ibm-supportedacimechanisms',
-  ]
+    OID.oid, [
+        '1.3.18.0.2.4.2482', # ibm-enabledCapabilities
+        '1.3.18.0.2.4.2481', # ibm-supportedCapabilities
+        'ibm-supportedacimechanisms',
+    ]
 )
 
 syntax_registry.registerAttrType(
-  DistinguishedName.oid, [
-    'ibm-adminid',
-  ]
+    DistinguishedName.oid, [
+        'ibm-adminid',
+    ]
 )
 
 syntax_registry.registerAttrType(
-  OctetString.oid, [
-    '1.3.18.0.2.4.3127', # ibm-slapdCryptoSalt,
-    '1.3.18.0.2.4.3116', # ibm-slapdCryptoSync
-  ]
+    OctetString.oid, [
+        '1.3.18.0.2.4.3127', # ibm-slapdCryptoSalt,
+        '1.3.18.0.2.4.3116', # ibm-slapdCryptoSync
+    ]
 )
 
 
