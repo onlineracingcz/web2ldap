1cc31c2cdb807d3909801695164308700677a8c0
Michael Ströder <michael@stroeder.com>
2019-01-08 18:01:43 +0100

    web2ldap.app.plugins.dyngroup: PEP-8

--- web2ldap/app/plugins/dyngroup.py
+++ web2ldap/app/plugins/dyngroup.py
@@ -4,49 +4,47 @@ web2ldap plugin classes for attributes defined for so-called dynamic groups
 
 from __future__ import absolute_import
 
-import ldap0,ldap0.ldapurl
+import ldap0
+import ldap0.ldapurl
 
 from web2ldap.ldaputil.base import is_dn
 
-from web2ldap.app.schema.syntaxes import LDAPUrl,syntax_registry
+from web2ldap.app.schema.syntaxes import LDAPUrl, syntax_registry
 
 
 class MemberUrl(LDAPUrl):
-  oid = 'MemberUrl-oid'
-  desc = 'LDAP URL describing search parameters used to lookup group members'
-  ldap_url = None
+    oid = 'MemberUrl-oid'
+    desc = 'LDAP URL describing search parameters used to lookup group members'
+    ldap_url = None
 
-  def __init__(self, sid, form, ls, dn, schema, attrType, attrValue, entry=None):
-    LDAPUrl.__init__(self, sid, form, ls, dn, schema, attrType, attrValue, entry)
+    def __init__(self, sid, form, ls, dn, schema, attrType, attrValue, entry=None):
+        LDAPUrl.__init__(self, sid, form, ls, dn, schema, attrType, attrValue, entry)
 
-  def _validate(self, attrValue):
-    try:
-      self.lu_obj = ldap0.ldapurl.LDAPUrl(attrValue)
-    except ValueError:
-      return 0
-    else:
-      search_base = self.lu_obj.dn.decode(self._ls.charset)
-      if not is_dn(search_base) or self.lu_obj.hostport:
-        return 0
-      else:
+    def _validate(self, attrValue):
         try:
-          # Try a dummy base-levelsearch with search base and filter string
-          # to provoke server-side errors
-          _ = self._ls.readEntry(
-            search_base,
-            attrtype_list=self.lu_obj.attrs,
-            search_filter=self.lu_obj.filterstr or '(objectClass=*)',
-          )
+            ldap_url = ldap0.ldapurl.LDAPUrl(attrValue)
+        except ValueError:
+            return False
+        search_base = ldap_url.dn.decode(self._ls.charset)
+        if not is_dn(search_base) or ldap_url.hostport:
+            return False
+        try:
+            # Try a dummy base-levelsearch with search base and filter string
+            # to provoke server-side errors
+            _ = self._ls.readEntry(
+                search_base,
+                attrtype_list=ldap_url.attrs,
+                search_filter=ldap_url.filterstr or '(objectClass=*)',
+            )
         except ldap0.LDAPError:
-          return False
-        else:
-          return True
+            return False
+        return True
 
 
 syntax_registry.registerAttrType(
-  MemberUrl.oid, [
-    '2.16.840.1.113730.3.1.198', # memberUrl
-  ]
+    MemberUrl.oid, [
+        '2.16.840.1.113730.3.1.198', # memberUrl
+    ]
 )
 
 
