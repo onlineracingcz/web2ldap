49442191fb9250905b4187e1758db55f385e7884
Michael Ströder <michael@stroeder.com>
2019-01-08 17:24:00 +0100

    web2ldap.app.plugins.pgpkeysrv: PEP-8

--- web2ldap/app/plugins/pgpkeysrv.py
+++ web2ldap/app/plugins/pgpkeysrv.py
@@ -8,55 +8,62 @@ from __future__ import absolute_import
 import re
 
 from web2ldap.app.schema.syntaxes import \
-  DirectoryString,GeneralizedTime,PreformattedMultilineText,DynamicValueSelectList,syntax_registry
+    DirectoryString, \
+    GeneralizedTime, \
+    PreformattedMultilineText, \
+    DynamicValueSelectList, \
+    syntax_registry
 
 
 syntax_registry.registerAttrType(
-  GeneralizedTime.oid, [
-    '1.3.6.1.4.1.3401.8.2.17', # pgpKeyCreateTime
-    '1.3.6.1.4.1.3401.8.2.22', # pgpKeyExpireTime
-  ]
+    GeneralizedTime.oid, [
+        '1.3.6.1.4.1.3401.8.2.17', # pgpKeyCreateTime
+        '1.3.6.1.4.1.3401.8.2.22', # pgpKeyExpireTime
+    ]
 )
 
 
 class PgpKey(PreformattedMultilineText):
-  oid = 'PgpKey-oid'
-  desc = 'PGP key'
-  reObj = re.compile('^-----BEGIN PGP PUBLIC KEY BLOCK-----[a-zA-Z0-9.: ()+/ =\n-]+-----END PGP PUBLIC KEY BLOCK-----$',re.S+re.M)
-  lineSep = '\n'
-  mimeType = 'application/pgp-keys'
-  cols = 64
+    oid = 'PgpKey-oid'
+    desc = 'PGP key'
+    reObj = re.compile(
+        '^-----BEGIN PGP PUBLIC KEY BLOCK-----[a-zA-Z0-9.: ()+/ =\n-]+-----END PGP PUBLIC KEY BLOCK-----$',
+        re.S+re.M,
+    )
+    lineSep = '\n'
+    mimeType = 'application/pgp-keys'
+    cols = 64
 
 syntax_registry.registerAttrType(
-  PgpKey.oid, [
-    '1.3.6.1.4.1.3401.8.2.11', # pgpKey
-  ]
+    PgpKey.oid, [
+        '1.3.6.1.4.1.3401.8.2.11', # pgpKey
+    ]
 )
 
 
 class PgpCertID(DirectoryString):
-  oid = 'PgpCertID-oid'
-  desc = 'PGP Cert ID'
-  reObj = re.compile('^[a-fA-F0-9]{16}$')
+    oid = 'PgpCertID-oid'
+    desc = 'PGP Cert ID'
+    reObj = re.compile('^[a-fA-F0-9]{16}$')
 
 syntax_registry.registerAttrType(
-  PgpCertID.oid, [
-    '1.3.6.1.4.1.3401.8.2.12', # pgpCertID
-  ]
+    PgpCertID.oid, [
+        '1.3.6.1.4.1.3401.8.2.12', # pgpCertID
+    ]
 )
 
 
-class OtherPgpCertID(DynamicValueSelectList,PgpCertID):
-  oid = 'OtherPgpCertID-oid'
-  ldap_url = 'ldap:///_?pgpCertID,pgpCertID?sub?(objectClass=pgpKeyInfo)'
+class OtherPgpCertID(DynamicValueSelectList, PgpCertID):
+    oid = 'OtherPgpCertID-oid'
+    ldap_url = 'ldap:///_?pgpCertID,pgpCertID?sub?(objectClass=pgpKeyInfo)'
 
-  def _validate(self, attrValue):
-    return PgpCertID._validate(self,attrValue)
+    def _validate(self, attrValue):
+        return PgpCertID._validate(self, attrValue)
 
 syntax_registry.registerAttrType(
-  OtherPgpCertID.oid, [
-    '1.3.6.1.4.1.3401.8.2.18', # pgpSignerID
-  ]
+    OtherPgpCertID.oid, [
+        '1.3.6.1.4.1.3401.8.2.18', # pgpSignerID
+    ]
 )
 
 
