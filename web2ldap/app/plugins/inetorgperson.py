ade6882ff13eab9d39f1f90e675740e79c1a974e
Michael Ströder <michael@stroeder.com>
2019-01-08 15:42:40 +0100

    web2ldap.app.plugins.inetorgperson: PEP-8

--- web2ldap/app/plugins/inetorgperson.py
+++ web2ldap/app/plugins/inetorgperson.py
@@ -9,31 +9,31 @@ but only if the structural object class of the entry is inetOrgPerson.
 
 from __future__ import absolute_import
 
-from web2ldap.app.schema.syntaxes import ComposedAttribute,DirectoryString,syntax_registry
+from web2ldap.app.schema.syntaxes import ComposedAttribute, DirectoryString, syntax_registry
 
 
-class CNInetOrgPerson(ComposedAttribute,DirectoryString):
-  oid = 'CNInetOrgPerson-oid'
-  desc = 'Attribute cn in object class inetOrgPerson'
-  maxValues = 1
-  compose_templates = (
-    '{givenName} {sn}',
-  )
+class CNInetOrgPerson(ComposedAttribute, DirectoryString):
+    oid = 'CNInetOrgPerson-oid'
+    desc = 'Attribute cn in object class inetOrgPerson'
+    maxValues = 1
+    compose_templates = (
+        '{givenName} {sn}',
+    )
 
 
-class DisplayNameInetOrgPerson(ComposedAttribute,DirectoryString):
-  oid = 'DisplayNameInetOrgPerson-oid'
-  desc = 'Attribute displayName in object class inetOrgPerson'
-  maxValues = 1
-  compose_templates = (
-    '{givenName} {sn} ({uid}/{employeeNumber})',
-    '{givenName} {sn} ({uid}/{uniqueIdentifier})',
-    '{givenName} {sn} ({employeeNumber})',
-    '{givenName} {sn} / {ou} ({departmentNumber})',
-    '{givenName} {sn} / {ou}',
-    '{givenName} {sn} ({uid})',
-    '{givenName} {sn}',
-  )
+class DisplayNameInetOrgPerson(ComposedAttribute, DirectoryString):
+    oid = 'DisplayNameInetOrgPerson-oid'
+    desc = 'Attribute displayName in object class inetOrgPerson'
+    maxValues = 1
+    compose_templates = (
+        '{givenName} {sn} ({uid}/{employeeNumber})',
+        '{givenName} {sn} ({uid}/{uniqueIdentifier})',
+        '{givenName} {sn} ({employeeNumber})',
+        '{givenName} {sn} / {ou} ({departmentNumber})',
+        '{givenName} {sn} / {ou}',
+        '{givenName} {sn} ({uid})',
+        '{givenName} {sn}',
+    )
 
 
 # Register all syntax classes in this module
