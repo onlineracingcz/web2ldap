ecb9c8aebabf8c8c78ebb4cc74c7cba9bef7067a
Michael Ströder <michael@stroeder.com>
2019-01-08 08:13:30 +0100

    web2ldap.app.plugins.sudoers: PEP-8 and other code-cleaning

--- web2ldap/app/plugins/sudoers.py
+++ web2ldap/app/plugins/sudoers.py
@@ -6,30 +6,34 @@ web2ldap plugin classes for sudo-ldap
 
 from __future__ import absolute_import
 
-from web2ldap.app.schema.syntaxes import DynamicValueSelectList,NotBefore,NotAfter,syntax_registry
+from web2ldap.app.schema.syntaxes import \
+    DynamicValueSelectList, \
+    NotBefore, \
+    NotAfter, \
+    syntax_registry
 
 
 class SudoUserGroup(DynamicValueSelectList):
-  oid = 'SudoUserGroup-oid'
-  desc = 'United Internet: sudoUser'
-  ldap_url = 'ldap:///_?cn,cn?sub?(objectClass=posixGroup)'
-  valuePrefix = '%'
+    oid = 'SudoUserGroup-oid'
+    desc = 'United Internet: sudoUser'
+    ldap_url = 'ldap:///_?cn,cn?sub?(objectClass=posixGroup)'
+    valuePrefix = '%'
 
 
 syntax_registry.registerAttrType(
-  NotBefore.oid, [
-    '1.3.6.1.4.1.15953.9.1.8', # sudoNotBefore
-  ]
+    NotBefore.oid, [
+        '1.3.6.1.4.1.15953.9.1.8', # sudoNotBefore
+    ]
 )
 
 
 syntax_registry.registerAttrType(
-  NotAfter.oid, [
-    '1.3.6.1.4.1.15953.9.1.9', # sudoNotAfter
-  ]
+    NotAfter.oid, [
+        '1.3.6.1.4.1.15953.9.1.9', # sudoNotAfter
+    ]
 )
 
 
 # Register all syntax classes in this module
 for symbol_name in dir():
-  syntax_registry.registerSyntaxClass(eval(symbol_name))
+    syntax_registry.registerSyntaxClass(eval(symbol_name))
