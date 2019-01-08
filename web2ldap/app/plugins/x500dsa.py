bfae3f323e7cbb7c42e2462b86467bffd60a1c1c
Michael Ströder <michael@stroeder.com>
2019-01-08 08:16:16 +0100

    web2ldap.app.plugins.x500dsa: PEP-8 and other code-cleaning

--- web2ldap/app/plugins/x500dsa.py
+++ web2ldap/app/plugins/x500dsa.py
@@ -5,13 +5,14 @@ web2ldap plugin classes for X.500 DSAs
 
 from __future__ import absolute_import
 
-from web2ldap.app.schema.syntaxes import OctetString,syntax_registry
+from web2ldap.app.schema.syntaxes import OctetString, syntax_registry
+
 
 class AccessControlInformation(OctetString):
-  oid = '1.3.6.1.4.1.1466.115.121.1.1'
-  desc = 'X.500: Access Control Information (ACI)'
+    oid = '1.3.6.1.4.1.1466.115.121.1.1'
+    desc = 'X.500: Access Control Information (ACI)'
+
 
 # Register all syntax classes in this module
 for name in dir():
-  syntax_registry.registerSyntaxClass(eval(name))
-
+    syntax_registry.registerSyntaxClass(eval(name))
