a26b3ba2cdfb0b3f4275dcd806273e60854945f0
Michael Ströder <michael@stroeder.com>
2019-01-08 18:26:42 +0100

    web2ldap.app.plugins.h350: PEP-8

--- web2ldap/app/plugins/h350.py
+++ web2ldap/app/plugins/h350.py
@@ -6,25 +6,25 @@ web2ldap plugin classes for H.350 Directory Services (see RFC 3944)
 from __future__ import absolute_import
 
 
-from web2ldap.app.schema.syntaxes import Uri,LDAPUrl,syntax_registry
+from web2ldap.app.schema.syntaxes import Uri, LDAPUrl, syntax_registry
 
 
 class CommURI(LDAPUrl):
-  oid = 'CommURI-oid'
-  desc = 'Labeled URI format to point to the distinguished name of the commUniqueId'
+    oid = 'CommURI-oid'
+    desc = 'Labeled URI format to point to the distinguished name of the commUniqueId'
 
 syntax_registry.registerAttrType(
-  CommURI.oid, [
-    '0.0.8.350.1.1.1.1.1', # commURI
-    '0.0.8.350.1.1.2.1.2', # commOwner
-  ]
+    CommURI.oid, [
+        '0.0.8.350.1.1.1.1.1', # commURI
+        '0.0.8.350.1.1.2.1.2', # commOwner
+    ]
 )
 
 
 syntax_registry.registerAttrType(
-  Uri.oid, [
-    '0.0.8.350.1.1.6.1.1', # SIPIdentitySIPURI
-  ]
+    Uri.oid, [
+        '0.0.8.350.1.1.6.1.1', # SIPIdentitySIPURI
+    ]
 )
 
 
