680378f4f13c78956ca46549607427d0439d60b7
Michael Ströder <michael@stroeder.com>
2019-01-12 19:33:26 +0100

    set SshPublicKey.input_pattern

--- web2ldap/app/plugins/opensshlpk.py
+++ web2ldap/app/plugins/opensshlpk.py
@@ -27,11 +27,12 @@ PARAMIKO_KEYCLASS = {
 class SshPublicKey(DirectoryString):
     oid = 'SshPublicKey-oid'
     desc = 'SSH public key of a user'
-    reObj = re.compile(
+    input_pattern = (
         '(^|.* )'
         '(ssh-rsa|ssh-dss|ecdsa-sha2-nistp256|ecdsa-sha2-nistp384|ecdsa-sha2-nistp521|ssh-ed25519)'
         ' (?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?(| .+)$'
     )
+    reObj = re.compile(input_pattern)
     # names of hash algorithms to be used when displaying fingerprint(s)
     hash_algorithms = ('md5', 'sha1', 'sha256', 'sha512')
     fileExt = 'pub'
