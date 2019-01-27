8c3c5d57e167d31bddcc4a0168da2d538f15586b
Michael Ströder <michael@stroeder.com>
2019-01-27 17:05:43 +0100

    code-cleaning in web2ldap.app.rename to make pylint happy

--- web2ldap/app/rename.py
+++ web2ldap/app/rename.py
@@ -30,8 +30,14 @@ from web2ldap.app.schema.viewer import schema_anchors
 
 
 def new_superior_field(app, sup_search_url, old_superior_dn):
+    """
+    returns Select field for choosing a new superior entry
+    """
 
     class NewSuperiorSelectList(web2ldap.app.schema.syntaxes.DynamicDNSelectList):
+        """
+        plugin class for choosing a new superior entry
+        """
         attr_value_dict = {
             u'': u'- Root Naming Context -',
         }
@@ -58,6 +64,9 @@ def new_superior_field(app, sup_search_url, old_superior_dn):
 
 
 def w2l_rename(app):
+    """
+    rename an entry
+    """
 
     rename_supsearchurl_cfg = app.cfg_param('rename_supsearchurl', {})
 
@@ -117,8 +126,13 @@ def w2l_rename(app):
             sup_search_url.filterstr = (
                 rename_newsupfilter or app.form.field['rename_newsupfilter'].default
             ).encode(app.ls.charset)
-            sup_search_url.dn = app.form.getInputValue('rename_searchroot', [''])[0].encode(app.ls.charset)
-            sup_search_url.scope = int(app.form.getInputValue('scope', [str(ldap0.SCOPE_SUBTREE)])[0])
+            sup_search_url.dn = app.form.getInputValue(
+                'rename_searchroot',
+                [''],
+            )[0].encode(app.ls.charset)
+            sup_search_url.scope = int(
+                app.form.getInputValue('scope', [str(ldap0.SCOPE_SUBTREE)])[0]
+            )
         else:
             sup_search_url = None
 
@@ -147,21 +161,27 @@ def w2l_rename(app):
             attrlist=['objectClass', 'structuralObjectClass', 'governingStructureRule'],
         )
         if not search_result:
-            raise web2ldap.app.core.ErrorExit(u'Empty search result when reading entry to be renamed.')
+            raise web2ldap.app.core.ErrorExit(
+                u'Empty search result when reading entry to be renamed.'
+            )
 
         entry = ldap0.schema.models.Entry(app.schema, app.ldap_dn, search_result)
 
         # Determine possible name forms for new RDN
         rdn_options = entry.get_rdn_templates()
         if rdn_options:
-            name_forms_text = '<p class="WarningMessage">Available name forms for RDN:<br>%s</p>' % (
+            name_forms_text = (
+                '<p class="WarningMessage">Available name forms for RDN:<br>%s</p>'
+            ) % (
                 '<br>'.join(rdn_options)
             )
         # Determine LDAP search filter for building a select list for new superior DN
         # based on governing structure rule
         dit_structure_ruleids = entry.get_possible_dit_structure_rules(app.ldap_dn)
         for dit_structure_ruleid in dit_structure_ruleids:
-            sup_structural_ruleids, sup_structural_oc = app.schema.get_superior_structural_oc_names(dit_structure_ruleid)
+            sup_structural_ruleids, sup_structural_oc = app.schema.get_superior_structural_oc_names(
+                dit_structure_ruleid
+            )
             if sup_structural_oc:
                 rename_newsupfilter_default = '(|%s)' % (
                     ''.join([
@@ -205,7 +225,9 @@ def w2l_rename(app):
             text_name_forms=name_forms_text,
             field_rename_supsearchurl=rename_supsearchurl_field.inputHTML(),
             value_rename_newsupfilter=app.form.utf2display(rename_newsupfilter_default),
-            field_rename_search_root=rename_search_root_field.inputHTML(default=rename_searchroot_default),
+            field_rename_search_root=rename_search_root_field.inputHTML(
+                default=rename_searchroot_default,
+            ),
             field_scope=app.form.field['scope'].inputHTML(default=scope_default),
             text_dit_structure_rule=dit_structure_rule_html,
         )
