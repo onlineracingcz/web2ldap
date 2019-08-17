37b080c39805fe8ae0ea01c7382ab031884d1e9e
Michael Ströder <michael@stroeder.com>
2019-08-17 18:52:18 +0200

    pin to ldap0 0.2.7

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     data_files=data_files,
     install_requires=[
         'setuptools',
-        'ldap0>=0.2.6',
+        'ldap0>=0.2.7',
         'ipaddress',
         'asn1crypto',
         'xlwt',
