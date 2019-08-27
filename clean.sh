f5ef4e1ad6d4a1e50360c1889845bded80120a34
Michael Ströder <michael@stroeder.com>
2019-08-27 20:51:25 +0200

    clean mypy cache

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python2 setup.py clean --all
-rm -r MANIFEST .coverage dist/web2ldap* build/* *.egg-info .tox docs/.build/*
+rm -r MANIFEST .coverage dist/web2ldap* build/* *.egg-info .tox docs/.build/* .mypy_cache
 rm web2ldap/*.py? web2ldap/*/*.py? tests/*.py? *.py?
 find -name "*.py?" -delete
 find -name __pycache__ | xargs -n1 -iname rm -r name
