a323f5cf39ca6c0c2cd833d7f3e9c335febedd75
Michael Ströder <michael@stroeder.com>
2019-08-29 12:11:56 +0200

    removed obsolete DN processing functions

--- web2ldap/ldaputil/__init__.py
+++ web2ldap/ldaputil/__init__.py
@@ -12,8 +12,6 @@ Apache License Version 2.0 (Apache-2.0)
 https://www.apache.org/licenses/LICENSE-2.0
 """
 
-from __future__ import absolute_import
-
 import re
 
 import ldap0
@@ -25,8 +23,6 @@ from . import passwd
 from . import extldapurl
 from . import oidreg
 
-
-SEARCH_SCOPE_STR = ('base', 'one', 'sub', 'subordinate')
 
 LDAP_OPT_NAMES_DICT = dict([
     (v, k)
@@ -70,152 +66,6 @@ def ietf_oid_str(oid):
                 raise ValueError('oid %r cannot be normalized' % (oid))
             r.append(vs)
     return '.'.join(r)
-
-
-def is_dn(s):
-    """returns 1 if s is a LDAP DN"""
-    assert isinstance(s, str), TypeError("Type of argument 's' must be str, was %r" % (s))
-    return ldap0.dn.is_dn(s.encode('utf-8'))
-
-
-def explode_rdn_attr(rdn):
-    """
-    explode_rdn_attr(attr_type_and_value) -> tuple
-
-    This function takes a single attribute type and value pair
-    describing a characteristic attribute forming part of a RDN
-    (e.g. u'cn=Michael Stroeder') and returns a 2-tuple
-    containing the attribute type and the attribute value unescaping
-    the attribute value according to RFC 2253 if necessary.
-    """
-    assert isinstance(rdn, str), TypeError("Argument 'rdn' must be str, was %r" % (rdn))
-    attr_type, attr_value = rdn.split(u'=', 1)
-    if attr_value:
-        r = []
-        start_pos = 0
-        i = 0
-        attr_value_len = len(attr_value)
-        while i < attr_value_len:
-            if attr_value[i] == u'\\':
-                r.append(attr_value[start_pos:i])
-                start_pos = i+1
-            i = i+1
-        r.append(attr_value[start_pos:i])
-        attr_value = u''.join(r)
-    return (attr_type, attr_value)
-
-
-def rdn_dict(dn):
-    assert isinstance(dn, str), TypeError("Argument 'dn' must be str, was %r" % (dn))
-    if not dn:
-        return {}
-    rdn, _ = split_rdn(dn)
-    if isinstance(rdn, str):
-        rdn = rdn.encode('utf-8')
-    result = {}
-    for i in ldap0.dn.explode_rdn(rdn.strip()):
-        attr_type, attr_value = explode_rdn_attr(i.decode('utf-8'))
-        if attr_type in result:
-            result[attr_type].append(attr_value)
-        else:
-            result[attr_type] = [attr_value]
-    return result
-
-
-def explode_dn(dn):
-    """
-    Unicode wrapper function for ldap0.dn.explode_dn() which returns [] for
-    a zero-length DN
-    """
-    assert isinstance(dn, str), TypeError("Argument 'dn' must be str, was %r" % (dn))
-    if not dn:
-        return []
-    return [
-        rdn.strip().decode('utf-8')
-        for rdn in ldap0.dn.explode_dn(dn.encode('utf-8').strip())
-    ]
-
-
-def normalize_dn(dn):
-    assert isinstance(dn, str), TypeError("Argument 'dn' must be str, was %r" % (dn))
-    return u','.join(explode_dn(dn))
-
-
-def matching_dn_components(dn1_components, dn2_components):
-    """
-    Returns how many levels of two distinguished names
-    dn1 and dn2 are matching.
-    """
-    if not dn1_components or not dn2_components:
-        return (0, u'')
-    # dn1_cmp has to be shorter than dn2_cmp
-    if len(dn1_components) <= len(dn2_components):
-        dn1_cmp, dn2_cmp = dn1_components, dn2_components
-    else:
-        dn1_cmp, dn2_cmp = dn2_components, dn1_components
-    i = 1
-    dn1_len = len(dn1_cmp)
-    while dn1_cmp[-i].lower() == dn2_cmp[-i].lower():
-        i = i+1
-        if i > dn1_len:
-            break
-    if i > 1:
-        return (i-1, u','.join(dn2_cmp[-i+1:]))
-    return (0, u'')
-
-
-def match_dn(dn1, dn2):
-    """
-    Returns how much levels of two distinguished names
-    dn1 and dn2 are matching.
-    """
-    return matching_dn_components(explode_dn(dn1), explode_dn(dn2))
-
-
-def match_dnlist(dn, dnlist):
-    """find best matching parent DN of dn in dnlist"""
-    dnlist = dnlist or []
-    dn_components = explode_dn(dn)
-    max_match_level, max_match_name = 0, u''
-    for dn_item in dnlist:
-        match_level, match_name = matching_dn_components(
-            explode_dn(dn_item),
-            dn_components
-        )
-        if match_level > max_match_level:
-            max_match_level, max_match_name = match_level, match_name
-    return max_match_name
-
-
-def parent_dn(dn):
-    """returns parent-DN of dn"""
-    dn_comp = explode_dn(dn)
-    if len(dn_comp) > 1:
-        return u','.join(dn_comp[1:])
-    elif len(dn_comp) == 1:
-        return u''
-    return None
-
-
-def parent_dn_list(dn, rootdn=u''):
-    """returns a list of parent-DNs of dn"""
-    result = []
-    dn_components = explode_dn(dn)
-    if rootdn:
-        max_level = len(dn_components) - len(explode_dn(rootdn))
-    else:
-        max_level = len(dn_components)
-    for i in range(1, max_level):
-        result.append(u','.join(dn_components[i:]))
-    return result
-
-
-def split_rdn(dn):
-    """returns tuple (RDN,base DN) of dn"""
-    if not dn:
-        raise ValueError('Empty DN cannot be split.')
-    dn_comp = explode_dn(dn)
-    return dn_comp[0], u','.join(dn_comp[1:])
 
 
 def logdb_filter(logdb_objectclass, dn, entry_uuid=None):
