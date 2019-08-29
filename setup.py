3d47ecbc01e0abcbfe0350449f20ad0eefb77432
Michael Ströder <michael@stroeder.com>
2019-08-29 19:24:13 +0200

    require ldap0>=0.3.8

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     data_files=data_files,
     install_requires=[
         'setuptools',
-        'ldap0>=0.3.6',
+        'ldap0>=0.3.8',
         'ipaddress',
         'asn1crypto',
         'xlwt',
