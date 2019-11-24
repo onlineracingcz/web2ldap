414f4fd2bf1950e1086903261dacc452c49658a8
Michael Ströder <michael@stroeder.com>
2019-11-24 19:43:02 +0100

    added release.sh

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Don't use uninitialized vars
+set -o nounset
+# Set strict umask
+umask 077
+
+# remove all temporary stuff
+./clean.sh
+
+# After here exit on any error
+set -e
+
+# determine version numer
+RELEASE=$(PYTHONPATH=$(pwd)/web2ldap python3 -c 'from __about__ import __version__ ; print(__version__)')
+
+echo "Will tag and publish ${RELEASE} now..."
+
+# push and tag the git repo
+git push
+git tag -s -m "release ${RELEASE}" "v${RELEASE}"
+git push --tags
+
+# build source distribution and push to PyPI
+# (release defined in setup.cfg)
+python3 setup.py release
