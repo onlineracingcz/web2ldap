6466799927b89801a0873a18f1173a91fa95f35f
Michael Ströder <michael@stroeder.com>
2019-12-30 02:17:07 +0100

    Support system-wide installation in /usr/local.

--- web2ldap/app/core.py
+++ web2ldap/app/core.py
@@ -19,6 +19,12 @@ import time
 import web2ldap.__about__
 from web2ldap.log import logger
 
+# prefixes considered to indicate system-wide installation outside a venv
+OS_SYS_PREFIXES = {
+    '/usr',
+    '/usr/local',
+}
+
 logger.info('Starting web2ldap %s', web2ldap.__about__.__version__)
 # this has to be done before import module package ldap0
 os.environ['LDAPNOINIT'] = '1'
@@ -33,8 +39,8 @@ check_inst()
 if 'WEB2LDAP_HOME' in os.environ:
     # env var points to web2ldap root directory
     etc_dir = os.path.join(os.environ['WEB2LDAP_HOME'], 'etc', 'web2ldap')
-elif os.name == 'posix' and sys.prefix == '/usr':
-    # OS-wide installation on POSIX platform (Linux, BSD, etc.)
+elif os.name == 'posix' and sys.prefix in OS_SYS_PREFIXES:
+    # assume OS-wide installation on POSIX platform (Linux, BSD, etc.)
     etc_dir = '/etc/web2ldap'
 else:
     # virtual env
