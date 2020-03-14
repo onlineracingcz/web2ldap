43f2cbd07b607a7ba1c9619c20bede8787158184
Michael Ströder <michael@stroeder.com>
2020-03-14 17:34:28 +0100

    (C) 2020

--- web2ldap/__about__.py
+++ web2ldap/__about__.py
@@ -23,7 +23,7 @@ __version_info__ = VersionInfo(
 __version__ = '.'.join(str(val) for val in __version_info__)
 __author__ = u'Michael Stroeder'
 __mail__ = u'michael@stroeder.com'
-__copyright__ = u'(C) 2017-2019 by Michael Ströder <michael@stroeder.com>'
+__copyright__ = u'(C) 2017-2020 by Michael Ströder <michael@stroeder.com>'
 __license__ = 'Apache-2.0'
 
 __all__ = [
