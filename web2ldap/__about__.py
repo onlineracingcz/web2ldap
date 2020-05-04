a02ee289ffe598293d14288fbb990ef989e2cba2
Michael Ströder <michael@stroeder.com>
2020-05-04 22:17:04 +0200

    started 1.5.72

--- web2ldap/__about__.py
+++ web2ldap/__about__.py
@@ -18,7 +18,7 @@ VersionInfo = collections.namedtuple('version_info', ('major', 'minor', 'micro')
 __version_info__ = VersionInfo(
     major=1,
     minor=5,
-    micro=71,
+    micro=72,
 )
 __version__ = '.'.join(str(val) for val in __version_info__)
 __author__ = 'Michael Stroeder'
