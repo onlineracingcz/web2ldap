817972d722e1c01ccb80337a52f8c3458280702b
Michael Ströder <michael@stroeder.com>
2020-05-30 16:48:39 +0200

    re-factored web2ldap.web.escape_html() to replace most special chars with str.translate()

--- web2ldap/web/__init__.py
+++ web2ldap/web/__init__.py
@@ -9,22 +9,15 @@ Apache License Version 2.0 (Apache-2.0)
 https://www.apache.org/licenses/LICENSE-2.0
 """
 
+# map for escape all HTML chars except ampersand "&"
+HTML_ESCAPE_MAP = {
+    ord(char): '&#%d;' % (ord(char))
+    for char in ('<', '>', "'", '"', ':', '=', '{', '}', '(', ',', '`')
+}
 
 def escape_html(val: str) -> str:
     """
     Escape all characters with a special meaning in HTML
     to appropriate character tags
     """
-    val = val.replace('&', '&#38;')
-    val = val.replace('<', '&#60;')
-    val = val.replace('>', '&#62;')
-    val = val.replace("'", '&#39;')
-    val = val.replace('"', '&#34;')
-    val = val.replace(':', '&#58;')
-    val = val.replace('=', '&#61;')
-    val = val.replace('{', '&#123;')
-    val = val.replace('}', '&#125;')
-    val = val.replace('(', '&#40;')
-    val = val.replace(')', '&#41;')
-    val = val.replace('`', '&#96;')
-    return val
+    return val.replace('&', '&#38;').translate(HTML_ESCAPE_MAP)
