3a83c4f9745c2088e04e8ce7b2d797e51b76ae6e
Michael Ströder <michael@stroeder.com>
2020-07-04 19:58:46 +0200

    Fixed str/bytes regression when handling PasswordPolicyException

--- web2ldap/app/handler.py
+++ web2ldap/app/handler.py
@@ -1029,7 +1029,7 @@ class AppHandler(LogHelper):
 
         except PasswordPolicyException as err:
             # Setup what's required for executing command 'passwd'
-            self.dn = (self.ls.l.whoami_s()[3:] or err.who).decode(self.ls.charset)
+            self.dn = self.ls.l.get_whoami_dn() or err.who
             # Output the change password form
             web2ldap.app.passwd.passwd_form(
                 self,
