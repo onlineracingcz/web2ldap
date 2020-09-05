1c09a825587f9f1fbe7371842f7ab927486e9ef1
Michael Ströder <michael@stroeder.com>
2020-09-05 12:25:10 +0200

    release 1.5.96: Fixed interop issue with gunicorn.

--- web2ldap/__about__.py
+++ web2ldap/__about__.py
@@ -18,7 +18,7 @@ VersionInfo = collections.namedtuple('version_info', ('major', 'minor', 'micro')
 __version_info__ = VersionInfo(
     major=1,
     minor=5,
-    micro=95,
+    micro=96,
 )
 __version__ = '.'.join(str(val) for val in __version_info__)
 __author__ = 'Michael Stroeder'
