a90695dced83771ff46446913b90651867bfdcec
Michael Ströder <michael@stroeder.com>
2021-11-16 14:12:04 +0100

    removed xargs arg -n1

--- clean.sh
+++ clean.sh
@@ -4,5 +4,5 @@ python3 setup.py clean --all
 rm -r MANIFEST .coverage dist/web2ldap* build/* *.egg-info .tox docs/.build/* .mypy_cache
 rm web2ldap/*.py? web2ldap/*/*.py? tests/*.py? *.py?
 find -name "*.py?" -delete
-find -name __pycache__ | xargs -n1 -iname rm -r name
+find -name __pycache__ | xargs -iname rm -r name
 rm -r slapdtest-[0-9]*
