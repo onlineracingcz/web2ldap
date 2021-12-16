ea371d2c5dd7ba454a83aaacafdf483b77a1624b
Michael Ströder <michael@stroeder.com>
2021-12-16 19:33:27 +0100

    use rm -f

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 python3 setup.py clean --all
-rm -r MANIFEST .coverage dist/web2ldap* build/* *.egg-info .tox docs/.build/* .mypy_cache
-rm web2ldap/*.py? web2ldap/*/*.py? tests/*.py? *.py?
+rm -rf MANIFEST .coverage dist/web2ldap* build/* *.egg-info .tox docs/.build/* .mypy_cache
+rm -f web2ldap/*.py? web2ldap/*/*.py? tests/*.py? *.py?
 find -name "*.py?" -delete
 find -name __pycache__ | xargs -iname rm -r name
-rm -r slapdtest-[0-9]*
+rm -rf slapdtest-[0-9]*
